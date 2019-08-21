b4228fb73d55f6d50fc10e867bee9ac2887b03a0
Justin Mc Nary <jmcnary@redventures.com>
2019-08-21 09:27:06 -0400

    Updates

---
+++ precache-manifest.af01b1cc782769e84e96b166e693c6f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c62906ebb86af0d3b280e35f30e377",
+    "url": "/dev-rolodex/index.html"
+  },
+  {
+    "revision": "111f2c83d6f10d73698c",
+    "url": "/dev-rolodex/static/css/main.533a3f8b.chunk.css"
+  },
+  {
+    "revision": "ebae56e1dc7493ba3e26",
+    "url": "/dev-rolodex/static/js/2.e1eafed3.chunk.js"
+  },
+  {
+    "revision": "111f2c83d6f10d73698c",
+    "url": "/dev-rolodex/static/js/main.7a60a101.chunk.js"
+  },
+  {
+    "revision": "3c773c36e095a5ce93df",
+    "url": "/dev-rolodex/static/js/runtime~main.3ee586d6.js"
+  }
+]);

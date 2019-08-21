a1605905f42b1f1850fdebbaf13c566e60b0f37a
Justin Mc Nary <jmcnary@redventures.com>
2019-08-21 09:23:50 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+import { Team } from './ac.team';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      team: Team,
+      searchField: '',
+      filter: '',
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  handleFilter = type => {
+    const filter = Team.filter(dev => dev.type.includes(type));
+    this.setState({ team: filter });
+  };
+
+  render() {
+    const { team, searchField } = this.state;
+    const filteredTeam = team.filter(dev =>
+      dev.name.toLowerCase().includes(searchField.toLocaleLowerCase()),
+    );
+
+    return (
+      <div className='App'>
+        <h1>ALLCONNECT DEVELOPERS</h1>
+        <SearchBox
+          placeholder='search by developer'
+          handleChange={this.handleChange}
+        />
+        <div className='button-container'>
+          <button onClick={() => this.handleFilter('back-end')}>
+            Back-End
+          </button>
+          <button onClick={() => this.handleFilter('front-end')}>
+            Front-End
+          </button>
+          <button onClick={() => this.setState({ team: Team })}>
+            Everybody
+          </button>
+        </div>
+        <CardList monsters={filteredTeam} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ac.team.js
@@ -0,0 +1,63 @@
+export const Team = [
+  {
+    name: 'Justin Mc Nary',
+    id: 'dev00',
+    email: 'jmcnary@redventures.com',
+    type: 'front-end',
+  },
+  {
+    name: 'Tom Zmyslo',
+    id: 'dev1',
+    email: 'tzmyslo@redventures.com',
+    type: 'front-end',
+  },
+  {
+    name: 'Bryan Chapman',
+    id: 'dev2',
+    email: 'bchapman@redventures.com',
+    type: 'front-end',
+  },
+  {
+    name: 'DUBB Sims',
+    id: 'dev3',
+    email: 'gwsims@redventures.com',
+    type: 'front-end',
+  },
+  {
+    name: 'Shaun Boehringer',
+    id: 'dev4',
+    email: 'sboehringer@redventures.com',
+    type: 'front-end',
+  },
+  {
+    name: 'Chas Vales',
+    id: 'dev5',
+    email: 'shvales@redventures.com',
+    type: 'front-end',
+  },
+  {
+    name: 'Joshua Soileau',
+    id: 'dev50',
+    email: 'jsoileau@redventures.com',
+    birthdate: '09/15/1989',
+    type: 'front-end',
+  },
+  {
+    name: 'Ryan NESSY Van Ess',
+    id: 'nessy',
+    email: 'rvaness@redventures.com',
+    type: 'front-end',
+  },
+  {
+    name: 'Carlos Silva',
+    id: 'backdev00',
+    email: 'casilva@redventures.com',
+    type: 'back-end',
+  },
+  {
+    name: 'Ryan Weber',
+    id: 'tank',
+    email: 'rweber@redventures.com',
+    type: 'back-end',
+  },
+];
